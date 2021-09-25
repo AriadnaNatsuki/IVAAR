2b7ae42c13fe7d8b12717e77c4752ed897442163
Elizabeth Colastra <elizabethcolastra@hotmail.es>
2021-09-25 15:20:38 +0200

    Title & meta description website and Route creation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 
+import { Route, Switch } from 'react-router';
 import './App.css';
 import './components/Navbar/Navbar'
 import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        {/* <Route path="/adopt" component={Adopt} />
+        <Route exact path="/adopt/:animalId" component={AnimalAdoption}/>
+        <Route path="/foster-homes" component={FosterHomes} />
+        <Route exact path="/foster-homes/:animalId" component={AnimalFoster} />
+        <Route path="/register" component={Register} />
+        <Route path="/login" component={Login}/> */}
+      </Switch>
     </div>
   );
 }
